3464eafdee97ba1edbb761bddedb1ce8069b2783
Grant Robert Thompson <58478508+Tower5954@users.noreply.github.com>
2021-03-05 03:55:46 +0000

    Add files via upload

--- webscraper_two.py
+++ webscraper_two.py
@@ -7,10 +7,18 @@ yc_webpage = response.text
 
 soup = BeautifulSoup(yc_webpage, "html.parser")
 
-article_tag = soup.find(name='a', class_='storylink')
-article_link = article_tag.get('href')
-article_upvote = soup.find(name='span', class_='score')
+articles = soup.find_all(name='a', class_='storylink')
 
-print(article_tag.getText())
-print(article_link)
-print(article_upvote.getText())
+article_texts = []
+article_links = []
+for article_tag in articles:
+    text = article_tag.get_text()
+    article_texts.append(text)
+    link = article_tag.get('href')
+    article_links.append(link)
+
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name='span', class_='score')]
+
+print(article_texts)
+print(article_links)
+print(article_upvotes)
